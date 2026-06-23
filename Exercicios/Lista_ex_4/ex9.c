84be54766c56b27ef651167e7212c644f399aafb
Matheus Kunz Bastiani <matheusbastiani08@gmail.com>
2026-06-23 11:14:46 -0300

    Aula 23/06

---
+++ Exercicios/Lista_ex_4/ex9.c
@@ -0,0 +1,26 @@
+#include <stdint.h>
+#include <stdio.h>
+
+typedef int32_t i32;
+
+i32 main (void){
+
+        i32 v [] = {1,1,1,2,1,1,1};
+        size_t tam = sizeof(v) / sizeof(v[0]);
+        i32 x = 0;
+
+        printf("Insira o valor de x: ");
+        scanf("%d", &x);
+
+        for(size_t i = 0;i < tam;i++){
+                for(size_t j = i + 1;j<tam;j++){
+                        if(v[i] + v[j] == x){
+                            printf("Verdadeiro");
+                            return 0;
+                        }
+            }
+        }
+        printf("Falso");
+
+        return 0;
+}
